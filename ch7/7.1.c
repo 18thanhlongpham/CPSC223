11fc266fa2713e66e7b5804160e74c4546548c34
18thanhlongpham <42815655+18thanhlongpham@users.noreply.github.com>
2020-05-03 18:16:53 -0700

    edit

--- ch7/7.1.c
+++ ch7/7.1.c
@@ -1,1 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(int argc, char *argv[]) {
+   int (*conv[2])(int) = { toupper, tolower };
+   int num;
+   char a;
+
+   if (argc >= 1) {
+       if (strcmp(argv[0], "./upper") == 0)
+           num = 1;
+       else if (strcmp(argv[0], "./lower") == 0)
+           num = 0;
+   }
+
+   while ((a = getchar()) != EOF)
+       putchar((conv[num])(a));
+
+   return 0;
+}
