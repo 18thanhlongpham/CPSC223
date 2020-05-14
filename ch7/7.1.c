e126f04ec17c239767c9de5b54f6830bfadfa11f
18thanhlongpham <42815655+18thanhlongpham@users.noreply.github.com>
2020-05-14 12:22:36 -0700

    Update 7.1.c

--- ch7/7.1.c
+++ ch7/7.1.c
@@ -2,21 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+void usage_error(int error_num){
+  fprintf(stderr, "Usage: ./lowerupper filename [lower|upper] \n");
+  exit(error_num);
+}
 int main(int argc, char *argv[]) {
-   int (*conv[2])(int) = { toupper, tolower };
-   int num;
-   char a;
-
-   if (argc >= 1) {
-       if (strcmp(argv[0], "./upper") == 0)
-           num = 1;
-       else if (strcmp(argv[0], "./lower") == 0)
-           num = 0;
+   char c;
+   
+   if(argc!= 1){
+      usage_error(1);
    }
-
-   while ((a = getchar()) != EOF)
-       putchar((conv[num])(a));
+   bool convert_to_upper= false;
+   bool convert_to_lower= false;
+   
+   char filename[BUFSIZ]; 
+   memset(filename, 0, BUFSIZ);
+   
+   strcpy(filename, argv[0]);
+   char* p= strrchr(filename, '/');
+   printf("filename is: %s\n", p+1);
+   
+   if(strcmp(p+1, "lower")== 0){
+      convert_to_lower= true;
+   }
+   if(strcmp(p+1, "upper")== 0){
+      convert_to_upper= true;
+   }
+   if(!convert_to_upper && !convert_to_lower){
+      usage_error(2);
+   }
+      
+   while((c= getchar())!= EOF){
+      c= convert_to_upper ? toupper(c) : tolower(c);
+      putchar(c);
+   }
 
    return 0;
 }
