816dca014dd15545158b17a91515f3b4c57ceb7c
18thanhlongpham <42815655+18thanhlongpham@users.noreply.github.com>
2020-05-14 19:47:55 -0700

    Update 7.8.c

--- ch7/7.8.c
+++ ch7/7.8.c
@@ -7,33 +7,28 @@
 
 void printpages(FILE *, FILE *);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
     FILE *fp;
     char *prog = argv[0];
     int linecount = 0;
     int num=1;
-    if (argc == 1) 
-    {
+    if (argc == 1){
         fprintf(stderr, "No files given\n");
         exit(0);
+    }else{
+      while (--argc > 0){
+        if ((fp = fopen(*++argv, "r")) == NULL) {
+            fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+            exit(1);
+        } else {
+            fprintf(stdout, "File %d: %s\n",num ,*argv);
+            printpages(fp, stdout);
+            fclose(fp);
+            num++;
+        }
+      }
     }
-    else
-        while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can't open %s\n",
-                        prog, *argv);
-                exit(1);
-            } else {
-                fprintf(stdout, "\n File %d: %s\n",num ,*argv);
-                printpages(fp, stdout);
-                fclose(fp);
-                num++;
-            }
 
-    if (ferror(stdout)) {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
-        exit(2);
-    }
     exit(0);
 }
 
@@ -48,13 +43,6 @@ void printpages(FILE *ifp, FILE *ofp)
         putc(c, ofp);
         if (c == '\n') {
             line = line + 1;
-            if (line == LINESPERPAGE)
-            {
-                fprintf(stdout, "\n\t\t\tPage %d End.\n\n", pg);
-                pg = pg + 1;
-                line = 0;
-            }
         }
-
     }
 }
